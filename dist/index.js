1ed86b9eeff9c6975f4f15897432db7175089767
Leonardo Schimock <dev.leonardosch@hotmail.com>
2026-05-17 04:01:00 -0300

    Update to date

--- dist/index.js
+++ dist/index.js
@@ -4,58 +4,80 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const readline_sync_1 = __importDefault(require("readline-sync"));
-/* function CalcularMedia(): void {
-    let primeiraNota: number;
-    let segundaNota: number;
-    let terceiraNota: number;
-    let media: number;
-    primeiraNota = Number(readlineSync.question('Digite a primeira nota: '));
-    segundaNota = Number(readlineSync.question('Digite a segunda nota: '));
-    terceiraNota = Number(readlineSync.question('Digite a terceira nota: '));
+function ParImpar() {
+    let numero;
+    numero = Number(readline_sync_1.default.question('Digite um numero: '));
+    if (numero % 2 === 0) {
+        console.log('\nNúmero par\n');
+    }
+    else {
+        console.log('\nNúmero ímpar\n');
+    }
+}
+function CalcularMedia() {
+    let primeiraNota;
+    let segundaNota;
+    let terceiraNota;
+    let media;
+    primeiraNota = Number(readline_sync_1.default.question('Digite a primeira nota: '));
+    segundaNota = Number(readline_sync_1.default.question('Digite a segunda nota: '));
+    terceiraNota = Number(readline_sync_1.default.question('Digite a terceira nota: '));
     media = (primeiraNota + segundaNota + terceiraNota) / 3;
     console.log(`\nA média das notas é: ${media}\n`);
 }
-CalcularMedia();
-
-
-function ContarVogais(): void {
-    let texto: string;
-    let quantidade: number = 0;
-    texto = readlineSync.question('Digite uma frase: ');
+function MaiorNumero() {
+    const numeros = [10, 45, 7, 99, 23];
+    let maior = numeros[0];
+    for (let i = 1; i < numeros.length; i++) {
+        if (numeros[i] > maior) {
+            maior = numeros[i];
+        }
+    }
+    console.log(`\nO maior número é: ${maior}\n`);
+}
+function ContarVogais() {
+    let texto;
+    let quantidade = 0;
+    texto = readline_sync_1.default.question('Digite uma frase: ');
     for (let i = 0; i < texto.length; i++) {
         let letra = texto[i].toLowerCase();
-        if (
-            letra === 'a' ||
+        if (letra === 'a' ||
             letra === 'e' ||
             letra === 'i' ||
             letra === 'o' ||
-            letra === 'u'
-        ) {
+            letra === 'u') {
             quantidade++;
         }
     }
     console.log(`\nQuantidade de vogais: ${quantidade}\n`);
 }
-ContarVogais(); */
-/* function Tabuada(): void {
-    let numero: number;
-    let resultado: number;
-    numero = Number(readlineSync.question('Digite um numero para exibir a tabuada: '));
-    for (let i = 1; i <= 10; i++) {
-        resultado = numero * i;
-        console.log(`${numero} x ${i} = ${resultado}`);
+function CalcularIMC() {
+    const peso = Number(readline_sync_1.default.question('Digite seu peso (kg): \n'));
+    const altura = Number(readline_sync_1.default.question('Digite sua altura (m): \n'));
+    const imc = peso / (altura * altura);
+    console.log(`Seu IMC é: ${imc.toFixed(2)}\n`);
+    if (imc < 18.5) {
+        console.log('Abaixo do peso\n');
+    }
+    else if (imc < 25) {
+        console.log('Peso normal\n');
+    }
+    else if (imc < 30) {
+        console.log('Sobrepeso\n');
+    }
+    else {
+        console.log('Obesidade\n');
     }
 }
-Tabuada(); */
-function validarSenha() {
-    let senha;
+function ValidarSenha() {
+    let password;
     do {
-        senha = readline_sync_1.default.question('Digite uma senha: ');
-        let temOitoCaracteres = senha.length >= 8;
-        let temMaiuscula = /[A-Z]/.test(senha);
-        let temMinuscula = /[a-z]/.test(senha);
-        let temNumero = /[0-9]/.test(senha);
-        if (temOitoCaracteres &&
+        password = readline_sync_1.default.question('Digite uma senha: ');
+        let temOitoChar = password.length >= 8;
+        let temMaiuscula = /[A-Z]/.test(password);
+        let temMinuscula = /[a-z]/.test(password);
+        let temNumero = /[0-9]/.test(password);
+        if (temOitoChar &&
             temMaiuscula &&
             temMinuscula &&
             temNumero) {
@@ -64,7 +86,7 @@ function validarSenha() {
         }
         else {
             console.log('\nSenha inválida!\n');
-            if (!temOitoCaracteres) {
+            if (!temOitoChar) {
                 console.log('- A senha deve ter pelo menos 8 caracteres');
             }
             if (!temMaiuscula) {
@@ -80,5 +102,58 @@ function validarSenha() {
         }
     } while (true);
 }
-validarSenha();
+function ContadorCaracteres() {
+    const texto = readline_sync_1.default.question('Digite um texto: \n');
+    const quantidade = texto.length;
+    console.log(`O texto possui ${quantidade} caracteres.\n`);
+}
+function Menu() {
+    console.log('\n===== MENU DE EXECICIOS =====');
+    console.log(' 2 - VERIFICAR PAR OU IMPAR');
+    console.log(' 3 - CALCULAR MEDIA DE 3 NOTAS');
+    console.log(' 7 - MAIOR NUMERO DO ARRAY');
+    console.log(' 8 - CONTADOR DE VOGAIS');
+    console.log(' 15 - CALCULADORA DE IMC');
+    console.log(' 16 - VALIDADOS DE SENHA');
+    console.log(' 18 - CONTADOR DE CARACTERES');
+    console.log(' 0 - SAIR');
+}
+// async e Promise void estão sendo usandos junto ao await para esperar uma resposta do usuario
+async function Sistema() {
+    let opcao;
+    //usamos o Do While para que o o menu seja executado 1 vez antes da verificaçao
+    do {
+        Menu();
+        opcao = readline_sync_1.default.question('Escolha uma opcao: ');
+        switch (opcao) {
+            case '2':
+                await ParImpar();
+                break;
+            case '3':
+                await CalcularMedia();
+                break;
+            case '7':
+                await MaiorNumero();
+                break;
+            case '8':
+                await ContarVogais();
+                break;
+            case '15':
+                await CalcularIMC();
+                break;
+            case '16':
+                await ValidarSenha();
+                break;
+            case '18':
+                await ContadorCaracteres();
+                break;
+            case '0':
+                console.log('Encerrando o programa...');
+                break;
+            default:
+                console.log('Opção inválido!');
+        }
+    } while (opcao !== '0');
+}
+Sistema();
 //# sourceMappingURL=index.js.map
