52b82efcf0afc162319aaf619c60c232dd1d6153
Leonardo Schimock <dev.leonardosch@hotmail.com>
2026-05-17 03:06:36 -0300

    first branch commit

--- dist/index.js
+++ dist/index.js
@@ -4,16 +4,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const readline_sync_1 = __importDefault(require("readline-sync"));
-function calcularMedia() {
-    let primeiraNota;
-    let segundaNota;
-    let terceiraNota;
-    let media;
-    primeiraNota = Number(readline_sync_1.default.question('Digite a primeira nota: '));
-    segundaNota = Number(readline_sync_1.default.question('Digite a segunda nota: '));
-    terceiraNota = Number(readline_sync_1.default.question('Digite a terceira nota: '));
-    media = (primeiraNota + segundaNota + terceiraNota) / 3;
-    console.log(`\nA média das notas é: ${media.toFixed(2)}\n`);
+//function CalcularMedia(): void {
+//    let primeiraNota: number;
+//    let segundaNota: number;
+//    let terceiraNota: number;
+//    let media: number;
+//    primeiraNota = Number(readlineSync.question('Digite a primeira nota: '));
+//    segundaNota = Number(readlineSync.question('Digite a segunda nota: '));
+//    terceiraNota = Number(readlineSync.question('Digite a terceira nota: '));
+//    media = (primeiraNota + segundaNota + terceiraNota) / 3;
+//    console.log(`\nA média das notas é: ${media}\n`);
+//}
+//CalcularMedia();
+function contarVogais() {
+    let texto;
+    let quantidade = 0;
+    texto = readline_sync_1.default.question('Digite uma frase: ');
+    for (let i = 0; i < texto.length; i++) {
+        let letra = texto[i].toLowerCase();
+        if (letra === 'a' ||
+            letra === 'e' ||
+            letra === 'i' ||
+            letra === 'o' ||
+            letra === 'u') {
+            quantidade++;
+        }
+    }
+    console.log(`\nQuantidade de vogais: ${quantidade}\n`);
 }
-calcularMedia();
+contarVogais();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,35 @@
 import readlineSync from 'readline-sync';
 
-function calcularMedia(): void {
-
+function CalcularMedia(): void {
     let primeiraNota: number;
     let segundaNota: number;
     let terceiraNota: number;
     let media: number;
-
     primeiraNota = Number(readlineSync.question('Digite a primeira nota: '));
     segundaNota = Number(readlineSync.question('Digite a segunda nota: '));
     terceiraNota = Number(readlineSync.question('Digite a terceira nota: '));
-
     media = (primeiraNota + segundaNota + terceiraNota) / 3;
-
-    console.log(`\nA média das notas é: ${media.toFixed(2)}\n`);
-
+    console.log(`\nA média das notas é: ${media}\n`);
 }
+CalcularMedia();
 
-calcularMedia();
+
+function ContarVogais(): void {
+    let texto: string;
+    let quantidade: number = 0;
+    texto = readlineSync.question('Digite uma frase: ');
+    for (let i = 0; i < texto.length; i++) {
+        let letra = texto[i].toLowerCase();
+        if (
+            letra === 'a' ||
+            letra === 'e' ||
+            letra === 'i' ||
+            letra === 'o' ||
+            letra === 'u'
+        ) {
+            quantidade++;
+        }
+    }
+    console.log(`\nQuantidade de vogais: ${quantidade}\n`);
+}
+ContarVogais();
